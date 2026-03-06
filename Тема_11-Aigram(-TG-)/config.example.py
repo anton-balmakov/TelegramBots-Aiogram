098b6e9101d793b2b8fa9d87d548b494efdc9333
Goga-ton <goga_ton@mail.ru>
2026-03-06 10:56:01 +0300

    Presentation on GitHub

---
+++ Тема_11-Aigram(-TG-)/config.example.py
@@ -0,0 +1,6 @@
+tltok = 'YOUR_TELEGRAM_BOT_TOKEN'
+api_key = 'YOUR_API_KEY' #https://home.openweathermap.org/api_keys - погода
+cat_apikey = 'YOUR_API_KEY'
+nasa_apikey = 'YOUR_API_KEY'
+dog_apikey = 'YOUR_API_KEY'
+exch_apikey = 'YOUR_API_KEY'
